085af89346e4b74de6a9007abdbfbcd96a2d1404
Rhyzko <valentin.martin@outlook.com>
2018-02-05 19:45:02 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,21 @@ app.get('/todos', (req, res) => {
     });
 });
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.send(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(todo) {
+            return res.status(200).send({todo});
+        } else {
+            return res.status(404).send();
+        }
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
 
 
 app.listen(3000, () => {
