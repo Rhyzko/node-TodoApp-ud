8b1f42f293b19092ead987b16ed3c1cfa981242e
Rhyzko <valentin.martin@outlook.com>
2018-02-08 00:45:54 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -117,6 +117,27 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 })
 
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+    res.status(400).send();
+  });
+  // User.findOne({email: body.email}).then((user) => {
+  //   if (!user) {
+  //     return Promise.reject();
+  //   }
+    
+    
+  // }).catch((e) => {
+  //   res.status(401).send(e);
+  // });
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
